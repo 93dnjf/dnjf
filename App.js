6bd17eb33bf0e2dfaa1bab15c7c6d8d810e1f971
93dnjf <93dnjf@gmail.com>
2019-11-26 22:03:26 +0900

    클릭 스타일 푸시

--- App.js
+++ App.js
@@ -6,20 +6,46 @@ import {
   StatusBar, 
   TextInput, 
   Dimensions, 
-  Platform } from 'react-native';
+  Platform,
+  ScrollView 
+} from 'react-native';
 
-const {height, width} = Dimensions.get("window");
+import ToDo from "./ToDo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Kawai To Do</Text>
-      <View style={styles.card}>
-          <TextInput style={styles.input} placeholder={"New To Do"}/>
+const { height, width } = Dimensions.get("window");
+
+export default class App extends React.Component {
+  state = {
+    newToDo: ""
+  }
+  render() {
+    const { newToDo } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+            <TextInput 
+                style={styles.input} 
+                placeholder={"New To Do"} 
+                value={newToDo} 
+                onChangeText={this._crontolNewToDo}
+                placeholderTextColor={"#999"}
+                returnKeyType={"done"}
+                autoCorrect={false}
+            />
+            <ScrollView contentContainerStyle={styles.toDos}>
+              <ToDo />
+            </ScrollView>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
+  _crontolNewToDo = text => {
+    this.setState({
+      newToDo: text
+    });
+  };
 }
 
 const styles = StyleSheet.create({
@@ -56,5 +82,14 @@ const styles = StyleSheet.create({
         elevation: 3
       }
     })
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: "#bbb", 
+    borderBottomWidth: 1,
+    fontSize: 25
+  },
+  toDos: {
+    alignItems: "center"
   }
 });
